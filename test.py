ad21a1cde9fba4bb7d3ff77b1a986c12f84b001a
hedizita <kovbanhezi@gmail.com>
2023-08-10 22:01:40 +0200

    idk

--- test.py
+++ test.py
@@ -10,28 +10,25 @@ rows = cur.fetchall()
 root = ET.Element('feed')
 
 for row in rows:
-    prod_id = row[0]
-    title = row[1]
-    desc = row[2]
-    link = row[3]
-    im_link = row[4]
-    add_im_link = row[5]
-    av = row[6]
-    price = row[7]
-    brand = row[8]
-    cond = row[9]
+    prod_id, title, desc, link, im_link, add_im_link, av, price, brand, cond = row  
 
-    root = ET.element('product')
-    root.set('id', prod_id)
-    root.set('title', title)
-    root.set('description', desc)
-    root.set('link', link)
-    root.set('image_link', im_link)
-    root.set('additional_image_link', add_im_link)
-    root.set('availability', av)
-    root.set('price', price)
-    root.set('brand', brand)
-    root.set('condition', cond)
+    prod_el = ET.SubElement(root, 'product')
+
+    ET.SubElement(prod_el, 'id').text = str(prod_id)
+    ET.SubElement(prod_el, 'title').text = title
+    ET.SubElement(prod_el, 'descripton').text = desc
+    ET.SubElement(prod_el, 'link').text = link
+    ET.SubElement(prod_el, 'image_link').text = im_link
+    ET.SubElement(prod_el, 'additional_image_link').text = add_im_link
+    ET.SubElement(prod_el, 'availability').text = av
+    ET.SubElement(prod_el, 'price').text = str(price)
+    ET.SubElement(prod_el, 'brand').text = brand
+    ET.SubElement(prod_el, 'condition').text = cond
 
 tree = ET.ElementTree(root)
-tree.write('feed.xml')
+tree.write('feed.xml', encoding='utf-8', xml_declaration=True)
+
+conn.close()
+
+print("ok")
+#tree.write('/HEDI/programozas/butopea/SIBPTC/feed.xml', encoding='utf-8', xml_declaration=True)
