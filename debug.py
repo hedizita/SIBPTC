3c2bd93c5d1b8b780ec4c28310c344c19b843e04
hedizita <kovbanhezi@gmail.com>
2023-08-13 14:06:43 +0200

    committed

--- debug.py
+++ debug.py
@@ -4,13 +4,7 @@ import xml.etree.ElementTree as ET
 conn = sqlite3.connect('data.sqlite')
 cur = conn.cursor()
 
-cur.execute('SELECT product.product_id, product.model, product.price, product.status, '
-            'product_description.name, product_description.description, '
-            'product_description.brand, ' 
-            '(SELECT product_image.image FROM product_image WHERE product_image.product_id = product.product_id) as image '
-            'FROM product '
-            'JOIN product_description ON product.product_id = product_description.product_id'
-            'JOIN manufacturer.name ON product.product_id = manufacturer.name.product_id')
+cur.execute("data.sqbpro")
 rows = cur.fetchall()
 
 root = ET.Element('feed')
