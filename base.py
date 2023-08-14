8a3579792d8f310a1148b50b62c5f604c26749e8
hedizita <kovbanhezi@gmail.com>
2023-08-14 22:34:39 +0200

    made comments

--- base.py
+++ base.py
@@ -1,9 +1,11 @@
 import sqlite3
 import xml.etree.ElementTree as ET
 
+#Establish a connection to the SQLite database
 conn = sqlite3.connect('data.sqlite')
 cur = conn.cursor()
 
+#Retrieve required fields from the database
 cur.execute('SELECT product.product_id, product.model, product.price, product.status, '
             'product_description.name, product_description.description, '
             '(SELECT product_image.image FROM product_image WHERE product_image.product_id = product.product_id) as image '
@@ -11,9 +13,11 @@ cur.execute('SELECT product.product_id, product.model, product.price, product.st
             'JOIN product_description ON product.product_id = product_description.product_id')
 rows = cur.fetchall()
 
+#Create the root element for the XML feed
 root = ET.Element('feed')
 root.set('xmlns', 'http://www.w3.org/2005/Atom')
 
+#Iterate through the fetched database rows and create the needed element and fields
 for row in rows:
     model, price, status, brand, title, desc, im_link = row  
 
@@ -28,9 +32,14 @@ for row in rows:
     ET.SubElement(prod_el, 'brand').text = brand
     ET.SubElement(prod_el, 'condition').text = 'new'
 
+#Create an ElementTree from the root element
 tree = ET.ElementTree(root)
+
+#Write the XML content to the 'feed.xml' file with specified encoding and declaration
 tree.write('feed.xml', encoding='utf-8', xml_declaration=True)
 
+#Close the database connection
 conn.close()
 
+#Print conformation message if succesful
 print("ok")
